9ce67e10da1d59103cba155ca4abe1ceca797455
Tomasz Banach <tomaszbanach01@gmail.com>
2017-12-08 15:08:00 +0100

    day 3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+/*
 
 var array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 console.log(array);
@@ -82,10 +82,74 @@ console.log(check);
 
 
 
-var numbers = [1,2,3,4,5];
-for(var i = 0; i <= 4; i++) {
+var numbers = [1,2,3,4,5,6,7,8];
+for(var i = 0; i <= numbers.length; i++) {
+    if (i === 1) break
     console.log(numbers[i])
 }
 
+var table = [1,2,3,4,5,6,7,8,9];
 
+console.log(table.push(10));
+console.log(table);
+
+var table1 = [1,2,3,4,5];
+console.log(table1);
+table1.unshift(0);
+table1.push(6);
+console.log(table1);
+var table2 = table1;
+table2.shift();
+table2.pop();
+console.log(table2);
+
+var concatedTable = table.concat(table2);
+console.log(concatedTable);
+console.log(table1);
+console.log(table);
+
+var sliceTable = ['zero', 'one', 'two', 'three']
+console.log(sliceTable.slice(-3, -2));
+
+
+var randomLength = (Math.round(Math.random()*10)+1)*2;
+console.log (randomLength);
+
+
+var array = [];
+for (var i = 0; i<randomLength; i++) {
+    array[i] = 'Index ' + i;
+}
+
+var halfLegth = array.length / 2;
+var firstHalf = array.slice(halfLegth -2, halfLegth +2);
+console.log(String(firstHalf));
+
+var array1 = [1,2,3,4,5,6,7,8,9,10];
+console.log(array1.indexOf(9))
+
+
+var obj = {a: 1};
+console.log(obj.a);
+
+
+var myCar = new Object();
+myCar.make = 'Toyota';
+myCar.model = 'Corolla';
+myCar['year'] = 2002;
+console.log(myCar);
+
+*/
+
+function addTwoNumbers(a,b){
+
+    return a + b;
+}
+console.log(addTwoNumbers(3,4));
+
+
+var sum = function summaryOfTwoNumbers(a,b){
+    return a + b;
+}
+console.log(sum(2,3));
 
